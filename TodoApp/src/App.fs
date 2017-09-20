eef70e9f6d76809d1b87d0fd5c85383e242156d9
Maxime Mangel <maxime.shadaen@protonmail.com>
2017-09-20 11:56:59 +0200

    Save TodoApp state

--- TodoApp/src/App.fs
+++ TodoApp/src/App.fs
@@ -10,72 +10,82 @@ open System
 
 let canvas = Browser.document.getElementById "application" :?> Browser.HTMLCanvasElement
 
-let mainWindow = { WindowInfo.Default with Width = 900.
+let mainWindow = { WindowInfo.Default with Width = 1024.
                                            Height = 600.
                                            Title = Some "Main window" }
 
-let mutable detailWindow : WindowInfo option = None
-
 let comboDisplay = { ComboInfo.Default with SelectedIndex = Some 0 }
 
-let newNote = InputInfo.Default
+type EditWindow =
+    { Window : WindowInfo
+      Input : InputInfo }
+
+    static member Create title =
+        { Window = { WindowInfo.Default with X = 50.
+                                             Y = 50.
+                                             Width = 500.
+                                             Height = 200.
+                                             Title = Some title
+                                             Draggable = true
+                                             Closable = true
+                                             Closed = true }
+          Input = { InputInfo.Default with Value = title } }
 
 type Note =
     { Title : string
-      Description : string
       Guid : Guid
       StateCheckbox : CheckboxInfo
-      DetailsWindow : WindowInfo }
+      EditWindow : EditWindow }
 
-    static member Create title =
+    static member Create (title, ?completed) =
         { Title = title
-          Description = ""
           Guid = Guid.NewGuid()
-          StateCheckbox = CheckboxInfo.Default
-          DetailsWindow = { WindowInfo.Default with X = 50.
-                                                    Y = 50.
-                                                    Width = 200.
-                                                    Height = 100.
-                                                    Title = Some title
-                                                    Draggable = true
-                                                    Closable = true
-                                                    Closed = true } }
+          StateCheckbox = { CheckboxInfo.Default with Value = defaultArg completed false }
+          EditWindow = EditWindow.Create title  }
 
-type Memory =
-    { mutable Notes : Note list }
+type AppState () as this =
+    let newNoteInput : InputInfo = InputInfo.Default
+    let mutable notes : Note list = []
+
+    do
+        newNoteInput.KeyboardCaptureHandler <-
+            Some (fun inputInfo keyboard ->
+                    match keyboard.Modifiers with
+                    | _ ->
+                        match keyboard.LastKey with
+                        | Keyboard.Keys.Enter ->
+                            if inputInfo.Value <> "" then
+                                this.Add this.NewNoteInput.Value
+                                this.NewNoteInput.Empty()
+                            true // Key has been captured
+                        | _ -> false )
+
+    member this.Notes
+        with get() = notes
+        and set(value) = notes <- value
+
+    member this.NewNoteInput = newNoteInput
+
+    member this.Add (title, ?completed) =
+        this.Notes <- Note.Create(title, ?completed = completed) :: this.Notes
 
     member this.Remove guid =
         this.Notes <-
             this.Notes
             |> List.filter(fun note -> note.Guid <> guid)
 
-    member this.Add title =
-        this.Notes <- Note.Create(newNote.Value) :: this.Notes
-
     member this.GetDetailsWindows () =
         this.Notes
         |> List.filter(fun note ->
-            not note.DetailsWindow.Closed
+            not note.EditWindow.Window.Closed
         )
 
-let memory = { Notes = [] }
+let appState = AppState()
 
-let keyboardCaptureHandler (keyboard: Keyboard.Record) =
-    match keyboard.Modifiers with
-    | _ ->
-        match keyboard.LastKey with
-        | Keyboard.Keys.Enter ->
-            if newNote.Value <> "" then
-                memory.Add newNote.Value
-                newNote.Value <- ""
-                newNote.Selection <- None
-                newNote.CursorOffset <- 0
-                newNote.TextStartOrigin <- 0
+appState.Add("Create a demo app for #FableConf", true)
+appState.Add("I am a really long note. So my text will be cut... That's really sad")
 
-            true // Key has been captured
-        | _ -> false
-
-let ui = Hink.Create(canvas, keyboardCaptureHandler = keyboardCaptureHandler)
+let ui = Hink.Create(canvas)
 
 let rec render (_: float) =
 
@@ -87,31 +97,45 @@ let rec render (_: float) =
     if ui.Window(mainWindow) then
         ui.Row([|1./6.; 3./6.; 1./6.; 1./6.|])
         ui.Empty()
-        ui.Input(newNote) |> ignore
+        ui.Input(appState.NewNoteInput) |> ignore
         ui.Combo(comboDisplay, ["All"; "Active"; "Completed"], None, labelAlign = Center)
         |> ignore
         ui.Empty()
         // Make space between input and list
         ui.Empty()
 
-    for note in memory.Notes do
-        ui.Row([| 1./8.; 5./8.; 1./8.; 1./8. |])
-        ui.Checkbox(note.StateCheckbox, "") |> ignore
+    for note in appState.Notes do
+        ui.Row([| 1./15.; 1./15.; 7./15.; 1./15.; 2./15.; 2./15.; 1./15. |])
+        ui.Empty()
+        ui.Checkbox(note.StateCheckbox) |> ignore
         ui.Label(note.Title)
+        ui.Empty()
 
-        if ui.Button("Show") then
-            note.DetailsWindow.Closed <- false
+        if ui.Button("Edit") then
+            note.EditWindow.Window.Closed <- false
 
         if ui.Button("Delete") then
-            memory.Remove(note.Guid)
+            appState.Remove(note.Guid)
 
-    for note in memory.GetDetailsWindows() do
-        if ui.Window(note.DetailsWindow, "purple") then
-            ui.Label("maxime")
+        ui.Empty()
 
+    for note in appState.GetDetailsWindows() do
+        if ui.Window(note.EditWindow.Window, "#9b59b6") then
+            ui.Label("Edit title:")
+            ui.Input(note.EditWindow.Input) |> ignore
+            ui.Empty()
+
+            ui.Row([|1./4.; 1./2.; 1./4.|])
+            ui.Empty()
+            if note.StateCheckbox.Value then
+                if ui.Button("Mark as active") then
+                    note.StateCheckbox.Value <- false
+            else
+                if ui.Button("Mark as completed") then
+                    note.StateCheckbox.Value <- true
+            ui.Empty()
 
     ui.Finish()
-
 
     Browser.window.requestAnimationFrame(Browser.FrameRequestCallback(render))
     |> ignore
